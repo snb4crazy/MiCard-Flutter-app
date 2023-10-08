a670b38b3b696f86adb9bd2c06ed56acd3b98f42
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-08 16:28:05 -0400

    Layout Challenge: Rows & Containers

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,40 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.yellow,
+                      height: 100.0,
+                      width: 100.0,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      height: 100.0,
+                      width: 100.0,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
